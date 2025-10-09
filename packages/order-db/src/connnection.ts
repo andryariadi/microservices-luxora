a800f1aa1ece4afd3af45c90a3d956e596e063bc
Andry Ariadi <andryariadi23@gmail.com>
2025-10-10 06:51:41 +0800

    feat(order-db): setup order mongoDB and create order schema

---
+++ packages/order-db/src/connnection.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectOrderDB = async () => {
+  if (isConnected) return;
+
+  if (!process.env.MONGODB_URI) throw new Error("MONGODB_URI is not defined in .env file!");
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    isConnected = true;
+    console.log("Connected to MongoDB!");
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
