62e67ce34c9c1774509cb9420946efccf4d29493
Andry Ariadi <andryariadi23@gmail.com>
2025-10-24 16:21:38 +0800

    feat(auth-service): create auth service

---
+++ apps/auth-service/src/index.ts
@@ -0,0 +1,48 @@
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
+import { authAdminMiddleware, authUserMiddleware } from "./middleware/authMiddleware";
+import userRoute from "./routes/user.route";
+import { producer } from "./utils/kafka";
+
+const app = express();
+const PORT = process.env.PORT || 8003;
+app.use(
+  cors({
+    origin: ["http://localhost:3003"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(clerkMiddleware());
+
+app.get("/health", (req: Request, res: Response) => {
+  res.status(200).json({ status: "Ok", uptime: process.uptime(), timestamp: Date.now() });
+});
+
+app.get("/clerk", authUserMiddleware, async (req: Request, res: Response) => {
+  res.status(200).json({ message: "Authenticated product service is running!", userId: req.userId });
+});
+
+app.use("/users", authAdminMiddleware, userRoute);
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.log(err, "<---errorInServer");
+
+  return res.status(err.status || 500).json({ message: err.message || "Internal Server Error!" });
+});
+
+const start = async () => {
+  try {
+    await producer.connect();
+
+    app.listen(PORT, () => {
+      console.log(`Product service is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Product service failed to start:", error);
+    process.exit(1);
+  }
+};
+
+start();
---
+++ apps/auth-service/src/middleware/authMiddleware.ts
@@ -0,0 +1,43 @@
+import { getAuth } from "@clerk/express";
+import { NextFunction, Request, Response } from "express";
+import { CustomJwtSessionClaims } from "@repo/types";
+
+declare global {
+  namespace Express {
+    interface Request {
+      userId?: string;
+    }
+  }
+}
+
+export const authUserMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+  const auth = getAuth(req);
+
+  if (!auth.userId) {
+    return res.status(401).json({ message: "User not authenticated!" });
+  }
+
+  req.userId = auth.userId;
+
+  return next();
+};
+
+export const authAdminMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+  const auth = getAuth(req);
+
+  if (!auth.userId) {
+    return res.status(401).json({ message: "User not authenticated!" });
+  }
+
+  const claims = auth.sessionClaims as CustomJwtSessionClaims;
+
+  console.log({ claims, auth: auth.userId }, "<---authMiddlewareExpress");
+
+  if (claims.metadata?.role !== "admin") {
+    return res.status(403).json({ message: "Unauthorized, you are not an admin!" });
+  }
+
+  req.userId = auth.userId;
+
+  return next();
+};
